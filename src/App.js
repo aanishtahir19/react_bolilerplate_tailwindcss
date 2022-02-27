3f1708eeb0993e938edd1670473db896efa3725b
aanishtahir19 <aanishtahir19@gmail.com>
2022-02-27 19:54:00 +0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+function App() {
+  return (
+    <>
+      <h1>Heading1</h1>
+      <h2>Heading2</h2>
+      <h3>Heading3</h3>
+      <p>Text1</p>
+      <section>
+        <h2>Heading2</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Necessitatibus iste hic, animi quam expedita recusandae illum
+          aspernatur quas laboriosam distinctio perspiciatis aut. Iusto natus
+          minus saepe. Pariatur et illum explicabo.
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab beatae
+          officiis cumque porro eveniet, laboriosam ipsam nam quasi doloremque,
+          qui, culpa dignissimos ipsum quis iste enim laudantium quas odio
+          magnam.
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
